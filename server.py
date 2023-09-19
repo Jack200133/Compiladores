e7d5566170f5fac0e8e61891a1851c45d40466b7
Angel Carrera <angelitocarrera@gmail.com>
2023-09-19 15:06:38 -0600

    add: Cors para front

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import base64
 import os
 import tkinter as tk
@@ -28,6 +29,7 @@ def build_tree(dot, node, parser, parent=None):
 # (importa otras bibliotecas necesarias aquí)
 
 app = Flask(__name__)
+CORS(app, origins=['http://localhost:5173', 'http://127.0.0.1:5173'])
 
 @app.route('/has_errors', methods=['POST'])
 def generate_images():
